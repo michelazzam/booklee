ed4093afb9ec69ed1d59109e28d9a51cf2e48261
abbaskheiraldeen <abbaskheiraldeen47@gmail.com>
2025-09-02 18:26:50 +0300

    added montserrat font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
-import '../global.css';
-
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { SplashScreen } from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -8,6 +9,23 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  // Keep splash screen until fonts are loaded
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
